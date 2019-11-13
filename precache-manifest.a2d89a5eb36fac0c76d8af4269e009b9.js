9f8aeaede692a7f406fdc5c5631e327b721aab13
Kamboj Prashant <prashant.kamboj@cerner.com>
2019-11-13 16:11:20 +0530

    Updates

---
+++ precache-manifest.a2d89a5eb36fac0c76d8af4269e009b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b40115c2dbac8951994d7fd02ef563",
+    "url": "/SampleProject/index.html"
+  },
+  {
+    "revision": "83e513ddaf8a3a042962",
+    "url": "/SampleProject/static/css/main.fc621f5f.chunk.css"
+  },
+  {
+    "revision": "4d8a07a2c6de66b3d24e",
+    "url": "/SampleProject/static/js/2.caaf4a6a.chunk.js"
+  },
+  {
+    "revision": "83e513ddaf8a3a042962",
+    "url": "/SampleProject/static/js/main.96d4639e.chunk.js"
+  },
+  {
+    "revision": "0b9d28a31c43ece41fa0",
+    "url": "/SampleProject/static/js/runtime-main.a1034443.js"
+  }
+]);
